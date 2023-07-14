0f4e72b8ddce19885113c8b5aa3f67934f2ca2d9
Carlos Alberto Jr <carlosalbertobzjr@gmail.com>
2023-07-14 03:56:40 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ import SocialLink from './components/social-link';
           <div className="md:w-3/5 lg:w-2/6 xl:2/4 flex flex-col justify-center items-center space-y-4 rounded">
             <img src={Photo} className="rounded-full border-4 border-slate-100 w-2/4" />
             <h1 className="text-3xl font-bold text-slate-100">Carlos Alberto Jr</h1>
-            <SocialLink href="" text="Linkedin" background="sky-800"backgroundHover="sky-900" icon={<FaLinkedin size={20}/>} />
-            <SocialLink text="Instagram" background="rose-600"backgroundHover="rose-700" icon={<FaInstagram size={20}/>} />
-            <SocialLink text="Github" background="slate-100"backgroundHover="slate-200" icon={<FaGithub size={20}/>} darkText/>
-            <SocialLink text="Twitter" background="sky-600"backgroundHover="sky-700" icon={<FaTwitter size={20}/>} />
+            <SocialLink href="https://www.linkedin.com/in/icarlosalbertojr" text="Linkedin" background="sky-800" backgroundHover="sky-900" icon={<FaLinkedin size={20}/>} />
+            <SocialLink href="https://www.instagram.com/icarlosalbertojr" text="Instagram" background="rose-600" backgroundHover="rose-700" icon={<FaInstagram size={20}/>} />
+            <SocialLink href="https://github.com/icarlosalbertojr" text="Github" background="slate-100" backgroundHover="slate-200" icon={<FaGithub size={20}/>} darkText/>
+            <SocialLink href="https://twitter.com/icarlosalbertjr" text="Twitter" background="sky-600" backgroundHover="sky-700" icon={<FaTwitter size={20}/>} />
           </div>
         </div>
     </div>
